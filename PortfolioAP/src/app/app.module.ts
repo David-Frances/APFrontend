85e84337990fba7d179af6e8a802711b77c0a88f
David2561 <davidtkf256@gmail.com>
2023-06-24 20:06:26 -0300

    añade header

--- PortfolioAP/src/app/app.module.ts
+++ PortfolioAP/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './portfolio/header/header.component';
+import { LogoAPComponent } from './portfolio/logo-ap/logo-ap.component';
+import { RedesComponent } from './portfolio/redes/redes.component';
+import { BannerComponent } from './portfolio/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    RedesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
